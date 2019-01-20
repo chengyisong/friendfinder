cb8be47ff9298e818ec5a82c9d10854b9457d3a8
CHENGYI SONG <carrie.song@beyondsoft.com>
2019-01-19 23:57:47 -0800

    fitst commit

---
+++ app/data/friendData.js
@@ -0,0 +1,13 @@
+var friendArray = [
+    {
+        name: "Carrie",
+        photo: "https://nathanaxephotography.com/image/71472-full_these-9-gifs-explain-the-flying-pig-perfectly.gif",
+        answers: [1, 4, 4, 2, 4, 5, 2, 1, 5, 3]
+    }
+];
+
+
+
+
+module.exports = friendArray;
+
